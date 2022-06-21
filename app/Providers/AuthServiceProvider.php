68f1d77cee08c81075f71ed2695679cb1a4102bb
sara-alaa <saraalaa9@gmail.com>
2022-06-21 17:24:55 +0200

    Register product polict

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Policies\ProductPolicy;
 use App\Policies\StorePolicy;
+use ECommerce\Product\Product;
 use ECommerce\Store\Store;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
@@ -17,6 +19,7 @@ class AuthServiceProvider extends ServiceProvider
      */
     protected $policies = [
         Store::class => StorePolicy::class,
+        Product::class => ProductPolicy::class,
     ];
 
     /**
