6b80f1ac3b0be0145cd949a13ba54bd07d41d06e
sara-alaa <saraalaa9@gmail.com>
2022-06-22 01:51:40 +0200

    create order policy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,10 @@
 
 namespace App\Providers;
 
+use App\Policies\OrderPolicy;
 use App\Policies\ProductPolicy;
 use App\Policies\StorePolicy;
+use ECommerce\Order\Order;
 use ECommerce\Product\Product;
 use ECommerce\Store\Store;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
@@ -20,6 +22,7 @@ class AuthServiceProvider extends ServiceProvider
     protected $policies = [
         Store::class => StorePolicy::class,
         Product::class => ProductPolicy::class,
+        Order::class => OrderPolicy::class,
     ];
 
     /**
